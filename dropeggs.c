6ed2aa7550d149d94125d6b1c71b7e930154ea79
xsabar <850296967@qq.com>
2023-11-08 21:59:24 +0800

    Update dropeggs.c
    add O(logn) algrithom

--- dropeggs.c
+++ dropeggs.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdint.h>
 
 int max(int a, int b) {
     return a > b ? a : b;
@@ -127,14 +128,14 @@ int dropeggs_dpmn(int eggs, int floors) {
 }
 
 /**
- * @brief O(mn)动态规划算法
+ * @brief O(n)动态规划算法
  * m-鸡蛋数，n-扔蛋次数，dp[m][n]-楼层数
  * dp[m][n]=dp[m-1][n-1]+dp[m][n-1]+1
  * @param eggs 
  * @param floors 
  * @return int 
  */
-int dropeggs_dpdrop(int eggs, int floors) {
+int dropeggs_dpn(int eggs, int floors) {
     int dp[2][floors + 1];
     int prev_idx = 0;
     int curr_idx = 1;
@@ -157,6 +158,71 @@ int dropeggs_dpdrop(int eggs, int floors) {
     return drop_cnt;
 }
 
+/**
+ * @brief O(m)算法计算最大楼层
+ * 递推公示：f(m,n)=C(n+1,m)+f(m-2,n)
+ * f(0,n)=0，f(1,n)=n
+ * @param m 鸡蛋数 
+ * @param n 扔蛋次数
+ * @return int 楼层数
+ */
+uint64_t calc_fmn(int m, int n) {
+    int odd = m & 1;
+    uint64_t floors = n * odd;
+    uint64_t comb = 1 + n * odd;
+    for (int i = 2 + odd; i <= m; i += 2) {
+        int min = n - i + 2;
+        if (min <= 0) {
+            break;
+        }
+        comb = (comb * (min + 1) / (i - 1)) * min / i;
+        floors += comb; 
+    }
+    return floors;
+}
+
+/**
+ * @brief O(m*n^(1/m))算法
+ * @param eggs 
+ * @param floors 
+ * @return int 
+ */
+int dropeggs_mnpowm(int eggs, int floors) {
+    int drop_cnt = ceil(log2(floors + 1));
+    while (calc_fmn(eggs, drop_cnt) < floors) {
+        ++drop_cnt;
+    }
+    return drop_cnt;
+}
+
+/**
+ * @brief O(logn)算法
+ * m-鸡蛋数，n-扔蛋次数，f(m,n)-楼层数
+ * 递推公示f(m,n)=C(n+1,m)+f(m-2,n)
+ * 假设鸡蛋数m无限，f(n)=2^n-1，因此当已知楼层数为N时，
+ * 扔鸡蛋的最小次数：n=ceil(log2(N + 1))
+ * @param eggs 
+ * @param floors 
+ * @return int 
+ */
+int dropeggs_logn(int eggs, int floors) {
+    int min_cnt = ceil(log2(floors + 1));
+    int max_cnt = min_cnt;
+    while (calc_fmn(eggs, max_cnt) < floors) {
+        max_cnt *= 2;
+    }
+    min_cnt = max(min_cnt, max_cnt / 2);
+    while (min_cnt + 1 < max_cnt) {
+        int drop_cnt = (min_cnt + max_cnt) / 2;
+        if (calc_fmn(eggs, drop_cnt) < floors) {
+            min_cnt = drop_cnt;
+        } else {
+            max_cnt = drop_cnt;
+        }
+    }
+    return max_cnt;
+}
+
 int main(int argc, char* argv[]) {
     int eggs = 0;   // 鸡蛋数
     int floors = 0; // 楼层数
@@ -173,6 +239,8 @@ int main(int argc, char* argv[]) {
     printf("dropeggs_dpmn2: %d\n", dropeggs_dpmn2(eggs, floors));
     printf("dropeggs_dpmnlogn: %d\n",dropeggs_dpmnlogn(eggs, floors));
     printf("dropeggs_dpmn: %d\n", dropeggs_dpmn(eggs, floors));
-    printf("dropeggs_dpdrop: %d\n", dropeggs_dpdrop(eggs, floors));
+    printf("dropeggs_dpn: %d\n",  dropeggs_dpn(eggs, floors));
+    printf("dropeggs_mnpowm: %d\n", dropeggs_mnpowm(eggs, floors));
+    printf("dropeggs_logn: %d\n", dropeggs_logn(eggs, floors));
     return 0;
 }
